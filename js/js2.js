d13f971a5c45612f1f38fb3affefe901c746d7c8
dah <52587973+Dah733@users.noreply.github.com>
2020-03-19 15:03:02 +0000

    projet tutore

--- js/js2.js
+++ js/js2.js
@@ -1,40 +1,153 @@
-jQuery(document).ready(function(){
-"use strict";
-  $('#j-slider').carouFredSel({
-    responsive:true,
-     width:'100%',
-      circular:true,
-      scroll:{
-         items:1,
-         duration:500,
-         pouseOnHover:true
 
-       },
-       auto:true,
-       items:{
-           visibler:{
-               min: 1,
-               max :1
-           },
-           height:"variable"
-           },
-        pagination:{
-            container:".sliderpager",
-            pageAnchorBuilder:false
-        }
-
-
-  });
-  $(window).scroll(function () {
-       var top = $(window).scrollTop();
-       if (top>=60) {
-           $("home").addClass('secondary');
-       }
-       else
-           if ($("home").hasClass('secondary')){
-             $("home").removeClass('secondary');
-           }
-  })
+/*$(function () {
+    var c = $("div.c");
+    var d = $("div.d");
+    var e = $("div.e");
+    var f = $("div.f");
+    var g = $("div.g");
+    var h = $("div.h");
+    var pi =$("                         <p class=\"text-center\" style='color: white'>\n" +
+        "                             Les principaux atouts du pays sont :<br>\n" +
+        "                             - Sa position géographique, à la croisée entre le Maghreb et l'Afrique Noire ;<br>\n" +
+        "                             - Un marché pétrolier en plein essor ;<br>\n" +
+        "                             - La mise en place d'une politique de privatisation et de libéralisation des échanges, en collaboration avec le Fonds Monétaire International ;<br>\n" +
+        "                             - La facilitation des IDE et l'égalité de traitement entre les investisseurs étrangers et nationaux.<br>\n" +
+        "\n" +
+        "                             Les mesures mises en place par le gouvernement:<br>\n" +
+        "                             Le gouvernement mauritanien a mis en place un système d eguichet unique pour faciliter les démarches des investisseurs<br>\n" +
+        "                             étrangers.<br>\n" +
+        "                             On peut y déposer les dossiers pour obtenir les licences, on y remplit les procédures administratives, on y\n" +
+        "                             établit les entreprises et on y obtient les permis de travail pour la main d'oeuvre étrangère. <br>Le gouvernement a révisé le\n" +
+        "                             Code des Investissements pour rendre la Mauritanie attractive.<br>\n" +
+        "                             Le code prévoit notamment le libre rapatriement des capitaux étrangers et des salaires des employés étrangers.<br>\n" +
+        "                             Le gouvernement a également établi des points francs ou des incitations à l'exportation. Les sociétés bénéficiant de<br>\n" +
+        "                             ce régime ne payent aucune taxe ou droit de douane.\n" +
+        "                         </p>\n");
 
 
-});
+    var pr =$(" <p class=\"text-center\">La liberté d'établissement\n" +
+        "                                 La liberté d'établissement est garantie par la loi\n" +
+        "                                 mauritanienne. <br>\n" +
+        "                                 Les privatisations et la libéralisation\n" +
+        "                                 du marché ont aussi permis de restaurer l'égalité entre\n" +
+        "                                 les sociétés et de garantir un accès égal au marché et\n" +
+        "                                 au crédit.\n" +
+        "\n" +
+        "                                 <br> La réglementation concernant les prises de participation\n" +
+        "                                 Les investisseurs étrangers peuvent prendre une\n" +
+        "                                 participation majoritaire au capital d'une entreprise\n" +
+        "                                 locale. <br>\n" +
+        "                                 Le code des investissements ne prévoit pas de\n" +
+        "                                 restriction fondée sur l'origine de l'investissement en\n" +
+        "                                 ce qui concerne l'accès au capital des entreprises en\n" +
+        "                                 Mauritanie.\n" +
+        "\n" +
+        "                                 <br>Demande d'autorisation spécifique\n" +
+        "                                 Il est possible d'investir dans tous les secteurs.<br>\n" +
+        "                                 Cependant dans le domaine de l'exploitation minière et\n" +
+        "                                 pétrolière, des télécoms, des assurances, des banques\n" +
+        "                                 et des télécommunications, des autorisations spécifiques\n" +
+        "                                 délivrées par le ministère auxquels ces secteurs sont\n" +
+        "                                 rattachés sont requises.<br> De même, dans le domaine de la\n" +
+        "                                 pêche, un investisseur étranger ne peut détenir plus de\n" +
+        "                                 49% des parts de la société.\n" +
+        "                             </p>");
+    var ad =$("<p className="text-center">\n" +
+        "- Deux ports de fret (Nouakchott et Nouadhibou);<br>\n" +
+
+       " - Port de pêche traditionnel;<br>\n" +
+
+        "- Aéroports internationaux<br>\n" +
+
+        "- Toutes les grandes villes sont reliées par des routes pavées (ce qui a permis de s'isoler des zones de \n" +
+        " production);<br>\n" +
+
+        "- L'électricité, le téléphone et Internet sont disponibles dans tous les centres urbains.<br>\n" +
+
+        "- Écoles de formation professionnelle (priorité pour l'enseignement des sciences et de la technologie);<br>\n" +
+
+        "- Hôpital dans chaque état et 4 hôpitaux à Nouakchott.<br>\n" +
+    "</p>");
+
+
+
+
+    var ga =$("<ol>
+        <li><h6>Garanties et protection de la propriété</h6><p>L'entreprise est garantie contre toute mesure de nationalisation, d'expropriation ou de
+    réquisition sur toute l'étendue du territoire national.</p></li>
+    <li><h6>Garantie de stabilité des conditions</h6><p>L'Etat garantit aux entreprises la stabilité des conditions juridiques, fiscales et
+    douanières accordées durant une période de vingt (20) ans à partir de la date
+    d'attribution du certificat d'investissement.</p></li>
+    <li><h6>Garantie de transferts des capitaux</h6><p>La liberté pour l'entreprise de transférer sans délai, après paiement des droits et taxes
+    mauritaniens, les revenus ou produits de toute nature, résultant de son exploitation, de
+    toute cession d'éléments d'actifs ou de sa liquidation, est garantie pour la part revenant
+    aux investisseurs étrangers conformément aux textes en vigueur.</p></li>
+    <li><h6>Garantie de transfert des rémunérations</h6><p>La liberté de transférer tout ou partie de sa rémunération salariale selon la
+    réglementation des changes en vigueur, après paiement des droits et taxes, quel qu'en
+    soit la nature juridique et le montant exprimé en monnaie locale ou en devises.</p></li>
+    <li><h6>Emploi du personnel expatrié</h6><p>L'entreprise peut employer des agents expatriés à des postes clés jusqu'à 10% du
+    personnel d'encadrement, conformément à la législation du travail en vigueur.</p></li>
+    <li><h6>Avantages douaniers</h6><ul>Les entreprises établies dans les zones franches bénéficient:
+    <li> de la franchise totale de droits et taxes douanier à l'importation des biens
+    d'équipement, matériels, véhicules utilitaires destinés à la production (la liste des
+    biens éligibles est fixée par Arrêté du Ministre des Finances).</li>
+    <li> de l'exonération de droits et taxes de douanes à l'exportation.</li></ul></li>
+    <li><h6>Champ d'application et seuils d'éligibilité</h6><ul>Peuvent faire l'objet de Conventions d'Etablissement, les investissements importants
+    dans les domaines de:
+        <li>l'activité de l'agriculture à l'exception de l'acquisition de terrains;</li>
+    <li>l'industrie à terre des produits de la pêche à l'exception de la farine de poissons;</li>
+    <li>l'activité de développement de la pêche artisanale et côtière;</li>
+    <li>Unités industrielles et manufacturières;<span><i class="fas fa-home"></i></span></li>
+    <li>Hôtellerie et tourisme hors de Nouakchott;</li>
+    </ul></li>
+
+    </ol>");
+
+     var sm =$("<div className="jumbotron">
+    <table className="table">
+        <tr className="a">
+            <td><b>Secteur d’activité</b></td>
+            <td><b>Investissement</b></td>
+            <td><b>Emplois directs</b></td>
+            <td><b>Emplois indirects</b></td>
+        </tr>
+        <tr>
+            <td><b>Agriculture</b></td>
+            <td>5 milliards d’UM</td>
+            <td>100</td>
+            <td>1000</td>
+            <tr>
+                <td><b>Transformation à terre des
+                    produits de la pêche
+                    industrielle à l’exception de la
+                    farine de poissons</b></td>
+                <td>5 milliards d’UM</td>
+                <td>500</td>
+                <td>2000</td>
+                <tr>
+                    <td><b>Pêche artisanale et côtière</b></td>
+                    <td>2 milliards d’UM</td>
+                    <td>100</td>
+                    <td>500</td>
+                </tr>
+                <tr>
+                    <td><b>Unités industrielles et
+                        manufacturières</b></td>
+                    <td>2 milliards d’UM</td>
+                    <td>50</td>
+                    <td>200</td>
+                </tr>
+                <tr>
+                    <td><b>Hôtellerie et Tourisme hors de
+                        Nouakchott</b></td>
+                    <td>500 millions d’UM</td>
+                    <td>20</td>
+                    <td>50</td>
+
+                </tr>
+            </tbody>
+    </table>
+</div>");
+
+
+*/
